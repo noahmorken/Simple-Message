6277772155829733e839a8496580362eb37a2143
nmorken <noahmorken@gmail.com>
2021-12-23 13:23:57 -0700

    Add files via upload

---
+++ simplemessage/simple_message.py
@@ -0,0 +1,2 @@
+message = "After the end of World War II, the world was split into two- East and West."
+print(message)
---
+++ simplemessage/simple_messages.py
@@ -0,0 +1,5 @@
+message = "Snake. We're not tools of the government, or anyone else. Fighting was the only thing... the only thing I was good at, but..."
+print(message)
+
+message = "At least I always fought for what I believed in... Snake... farewell."
+print(message)
